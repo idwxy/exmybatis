7292f769ac1392c9b9283f6cf6aefbd14d7ccba5
idwxy <idwxy@live.com>
2019-09-08 22:07:22 +0800

    first commit

---
+++ src/main/java/com/idwxy/exmybatis/ExmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.idwxy.exmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExmybatisApplication.class, args);
+	}
+
+}
