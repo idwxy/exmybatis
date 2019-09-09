23b481c197a9b7018cd83a9d5c4eee90d51e74af
idwxy <idwxy@live.com>
2019-09-10 00:48:23 +0800

    增加 mapper 层注解

--- src/main/java/com/idwxy/exmybatis/ExmybatisApplication.java
+++ src/main/java/com/idwxy/exmybatis/ExmybatisApplication.java
@@ -1,9 +1,12 @@
 package com.idwxy.exmybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring boot 主入口, @MapperScan 可以指定扫描 mapper 类包路径，减少了每个 mapper 类都要加 @Mapper 注解
 @SpringBootApplication
+@MapperScan("com.idwxy.exmybatis.dao")
 public class ExmybatisApplication {
 
 	public static void main(String[] args) {
