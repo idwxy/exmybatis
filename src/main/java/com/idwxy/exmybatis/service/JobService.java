c0c7986b785b92906211ff2eaeadd605e0697e8b
idwxy <idwxy@live.com>
2019-09-09 22:41:35 +0800

    新增 JobService

---
+++ src/main/java/com/idwxy/exmybatis/service/JobService.java
@@ -0,0 +1,42 @@
+package com.idwxy.exmybatis.service;
+
+import com.idwxy.exmybatis.entity.Job;
+
+import java.util.List;
+
+public interface JobService {
+
+    /**
+     * 创建 job
+     * @param job
+     * @return
+     */
+    int insertJob(Job job);
+
+    /**
+     * 删除 job
+     * @param id
+     * @return
+     */
+    int deleceJob(Integer id);
+
+    /**
+     * 更新 job
+     * @param job
+     * @return
+     */
+    int updateJob(Job job);
+
+    /**
+     * 通过 id 查找 job
+     * @param id
+     * @return
+     */
+    Job selectJobById(Integer id);
+
+    /**
+     * 查找所有 job
+     * @return
+     */
+    List<Job> seleceAllJob();
+}
